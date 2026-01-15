4ac07c32c1f3c6fc5b45e3ff2986f0ab62b91f98
dragonflyllj <dragonflyllj@gmail.com>
2026-01-15 21:04:45 +0900

    navbar

--- script.js
+++ script.js
@@ -10,3 +10,9 @@ const nav = document.querySelector(".nav");
 toggleButton.addEventListener("click", () => {
   nav.classList.toggle("active");
 });
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", () => {
+  navLinks.classList.toggle("open");
+});
